1004dc40d0799d3372ec2563085162ae922cead7
Thien Ho <thien.hophu@gmail.com>
2021-10-02 00:30:17 +0700

    init go-react with docker

---
+++ README.md
@@ -0,0 +1,11 @@
+# Build Docker at local
+
+docker build -t go-react .
+
+# Run Docker at local
+
+docker run --rm -d --name go-react -p 3000:3000 go-react
+
+# Stop Docker at Local
+
+docker container stop go-react
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+
+	r.Run(":3000")
+	// r.Run()
+}
