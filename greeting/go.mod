0c9522c3891b71aec2f3c02ab231f098cdab6f21
Thien Ho <thien.hophu@gmail.com>
2021-10-03 21:07:21 +0700

    learning-go

---
+++ greeting/go.mod
@@ -0,0 +1,3 @@
+module github.com/thienhophu/go-react/greeting
+
+go 1.17
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/thienhophu/go-react/greeting"
 )
 
 func main() {
+	log.SetPrefix(("greeting: "))
+	log.SetFlags(0)
+
+	message, err := greeting.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+
 	r := gin.Default()
 
 	r.Use(static.Serve("/", static.LocalFile("./public", true)))
 
-	// r.Run(":3000")
-	r.Run()
+	// r.Run()
+	r.Run(":3000")
 }
